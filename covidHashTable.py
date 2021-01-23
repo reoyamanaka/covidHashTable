33825edc23ed486b5cff3da0d5eb65c0f0895865
Reo Yamanaka <ryamanaka807@gmail.com>
2021-01-22 23:55:39 -0500

    automatically calculate faster process and present conclusion

--- covidHashTable.py
+++ covidHashTable.py
@@ -1,7 +1,7 @@
 import time, requests
 
 fsasCases = {}
-
+speeds = {"List":0, "Hash Table":0}
 url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/download_resource/e5bf35bc-e681-43da-b2ce-0242d00922ad?format=json"
 resp = requests.get(url)
 for obj in resp.json():
@@ -16,10 +16,12 @@ start_time = time.time()
 for fsa in fsasCases.keys():
     if fsa == fsaOfInterest: 
         print("The number of ACTIVE cases in the FSA is %d."%fsasCases[fsa])
-print("This process, which used a list, took --- %s seconds ---." %(time.time() - start_time))
+speeds["List"] = time.time() - start_time
+print("This process, which used a list, took --- %s seconds ---." %(speeds["List"]))
 
 start_time = time.time()
 print("The number of ACTIVE cases in the FSA is %d."%fsasCases[fsaOfInterest])
-print("This process, which used a hash table, took --- %s seconds ---." % (time.time() - start_time))
-
-    
+speeds["Hash Table"] = time.time() - start_time
+print("This process, which used a hash table, took --- %s seconds ---." % (speeds["Hash Table"]))
+fasterProcess = min(speeds)
+print("\nThe %s was faster retrieving the data."%fasterProcess)
